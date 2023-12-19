680cf19816926989bba158b2158ec4bdd537097b
Chandong <kcd9511@gmail.com>
2023-12-19 22:41:56 +0900

    todo.tsx파일 추가

---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+const TodoApp: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (text: string) => {
+    setTodos([...todos, { id: todos.length + 1, text, completed: false }]);
+  };
+
+  return (
+    <div>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+      <button onClick={() => addTodo('New Todo')}>Add Todo</button>
+    </div>
+  );
+};
+
+export default TodoApp;
