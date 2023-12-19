67daa57e7fcd87c9ae1de8b3c62c07ad2bb73ee8
Chandong <kcd9511@gmail.com>
2023-12-19 22:54:30 +0900

    투두리스 체크박스 추가

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,9 +1,11 @@
+// src/components/TodoApp.tsx
+
 import React, { useState } from 'react';
 
 interface Todo {
   id: number;
   text: string;
-  completed: boolean;
+  completed: boolean; // 새로 추가된 필드
 }
 
 const TodoApp: React.FC = () => {
@@ -13,11 +15,26 @@ const TodoApp: React.FC = () => {
     setTodos([...todos, { id: todos.length + 1, text, completed: false }]);
   };
 
+  const toggleTodo = (id: number) => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
   return (
     <div>
       <ul>
         {todos.map(todo => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(todo.id)}
+            />
+            {todo.text}
+          </li>
         ))}
       </ul>
       <button onClick={() => addTodo('New Todo')}>Add Todo</button>
