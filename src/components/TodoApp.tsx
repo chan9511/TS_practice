d3b27ade1edcb19828bfbd551254d2fe940a5785
Chandong <kcd9511@gmail.com>
2023-12-20 14:14:13 +0900

    'Enter활성화'

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -6,15 +6,17 @@ interface Todo {
   id: number;
   text: string;
   completed: boolean;
+  category: string;
 }
 
 const TodoApp: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [editing, setEditing] = useState<number | null>(null);
   const [newText, setNewText] = useState('');
+  const [newCategory, setNewCategory] = useState('');
 
-  const addTodo = (text: string) => {
-    setTodos([...todos, { id: todos.length + 1, text, completed: false }]);
+  const addTodo = (text: string, category: string) => {
+    setTodos([...todos, { id: todos.length + 1, text, completed: false, category }]);
   };
 
   const toggleTodo = (id: number) => {
@@ -33,15 +35,22 @@ const TodoApp: React.FC = () => {
     setEditing(id);
     const todoToEdit = todos.find(todo => todo.id === id);
     setNewText(todoToEdit?.text || '');
+    setNewCategory(todoToEdit?.category || '');
   };
 
   const finishEditing = () => {
     setEditing(null);
     setTodos(prevTodos =>
       prevTodos.map(todo =>
-        todo.id === editing ? { ...todo, text: newText } : todo
+        todo.id === editing ? { ...todo, text: newText, category: newCategory } : todo
       )
     );
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      finishEditing();
+    }
   };
 
   return (
@@ -55,6 +64,13 @@ const TodoApp: React.FC = () => {
                   type="text"
                   value={newText}
                   onChange={e => setNewText(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                />
+                <input
+                  type="text"
+                  value={newCategory}
+                  onChange={e => setNewCategory(e.target.value)}
+                  onKeyDown={handleKeyDown}
                 />
                 <button onClick={finishEditing}>Save</button>
               </>
@@ -65,7 +81,7 @@ const TodoApp: React.FC = () => {
                   checked={todo.completed}
                   onChange={() => toggleTodo(todo.id)}
                 />
-                {todo.text}
+                {`${todo.text} (${todo.category})`}
                 <button onClick={() => startEditing(todo.id)}>Edit</button>
                 <button onClick={() => deleteTodo(todo.id)}>Delete</button>
               </>
@@ -73,7 +89,7 @@ const TodoApp: React.FC = () => {
           </li>
         ))}
       </ul>
-      <button onClick={() => addTodo('New Todo')}>Add Todo</button>
+      <button onClick={() => addTodo('해야 할 일', '종류')}>할 일 추가</button>
     </div>
   );
 };
